c2804beacaca9cbe0c627839747ba07d1a0d4d8f
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-12-04 16:07:37 +0800

    Create InnerEnergySmelter.java

---
+++ src/immersionIndustry/types/innerenergy/blocks/production/InnerEnergySmelter.java
@@ -0,0 +1,72 @@
+package immersionIndustry.types.innerenergy.blocks.production;
+
+import arc.*;
+import arc.scene.*;
+import arc.scene.style.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.Vec2;
+import arc.struct.*;
+import arc.util.*;
+import arc.util.io.*;
+import arc.scene.ui.layout.Table;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.units.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.logic.*;
+import mindustry.type.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+import static arc.graphics.g2d.Draw.rect;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+
+import immersionIndustry.IMShaders;
+import immersionIndustry.IMColors;
+import immersionIndustry.contents.IMFx;
+import immersionIndustry.types.innerenergy.blocks.*;
+import immersionIndustry.types.innerenergy.blocks.InnerenergyBlock.*;
+
+public class InnerEnergySmelter extends InnerenergyBlock {
+  
+  public InnerEnergySmelter(String name) {
+    super(name);
+    solid = true;
+    hasItems = true;
+    ambientSound = Sounds.machine;
+    sync = true;
+    ambientSoundVolume = 0.03f;
+    flags = EnumSet.of(BlockFlag.factory);
+  }
+  
+  @Override
+  public void init() {
+    IMShaders.fuse.setUniformf("u_texture",id);
+    region.bind(id);
+  }
+  
+  public class SmelterBuild extends InnerenergyBuilding {
+    
+    @Override
+    public void draw() {
+      super.draw();
+      Draw.shader(IMShaders.fuse);
+      Draw.color(Pal.accent);
+      Fill.square(x, y, block.size * tilesize / 2f + 1f);
+      Draw.reset();
+      Draw.shader();
+    }
+    
+  }
+  
+}
