404ecc2ca443d8d5bf877e108e8b12c33c008ce6
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-12-04 16:45:34 +0800

    Add files via upload

--- src/immersionIndustry/contents/IMShaders.java
+++ src/immersionIndustry/contents/IMShaders.java
@@ -28,19 +28,16 @@ public class IMShaders {
   }
   
   public static class FuseShader extends ModShader {
+    
+    public TextureRegion region;
+    Texture noiseTex;
+    
     public FuseShader() {
       super("fuseShader","fuseShader");
+      loadNoise();
     }
-  }
-  
-  public static class ModShader extends Shader {
-    Texture noiseTex;
-    public ModShader(String vert, String frag){
-			super(getShaderFi(vert + ".vert"), getShaderFi(frag + ".frag"));
-			loadNoise();
-		}
-		
-		public String textureName(){
+    
+    public String textureName(){
       return "noise";
     }
     
@@ -53,6 +50,8 @@ public class IMShaders {
 		
 		@Override
 		public void apply() {
+		  region.texture.bind(0);
+      setUniformf("u_texture",0);
 		  setUniformf("u_campos", Core.camera.position.x - Core.camera.width / 2, Core.camera.position.y - Core.camera.height / 2);
       setUniformf("u_resolution", Core.camera.width, Core.camera.height);
       setUniformf("u_time", Time.time);
@@ -67,6 +66,13 @@ public class IMShaders {
 
         setUniformi("u_noise", 1);
       }
+		}
+  }
+  
+  public static class ModShader extends Shader {
+
+    public ModShader(String vert, String frag){
+			super(getShaderFi(vert + ".vert"), getShaderFi(frag + ".frag"));
 		}
   }
   
