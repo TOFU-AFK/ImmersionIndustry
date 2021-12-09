8ed8831309353b4cb604e0dccbed14e29725528e
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-12-09 22:37:47 +0800

    Add files via upload

--- src/immersionIndustry/types/innerenergy/blocks/production/InnerEnergySmelter.java
+++ src/immersionIndustry/types/innerenergy/blocks/production/InnerEnergySmelter.java
@@ -66,12 +66,12 @@ public class InnerEnergySmelter extends InnerenergyBlock {
     
     @Override
     public void drawConfigure() {
-      vbar.draw(x,y);
+      vbar.draw(x + block.size * tilesize,y);
     }
     
     @Override
     public void buildConfiguration(Table table) {
-      vbar = new Vbar(block.size * block.size * tilesize);
+      vbar = new Vbar(block.size * tilesize);
       table.button("添加",() -> {
         vbar.add(IMColors.colorPrimary,"熔融坍缩量",100);
         vbar.add(IMColors.colorPrimary,"熔融铜",200);
