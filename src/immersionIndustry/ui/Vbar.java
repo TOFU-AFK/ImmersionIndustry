1ff8bb4967259b85a535ae25ec2801fc2ca07b83
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-12-19 10:10:53 +0800

    Add files via upload

--- src/immersionIndustry/ui/Vbar.java
+++ src/immersionIndustry/ui/Vbar.java
@@ -7,18 +7,19 @@ import arc.util.pooling.*;
 import arc.graphics.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
+import arc.graphics.g2d.*;
 import mindustry.gen.*;
 import mindustry.net.Administration.*;
 import mindustry.ui.dialogs.*;
 import mindustry.ui.*;
 import mindustry.graphics.*;
+import mindustry.graphics.Shaders;
 
 import static mindustry.Vars.*;
 import static arc.graphics.g2d.Draw.rect;
 import static arc.graphics.g2d.Draw.*;
 import static arc.graphics.g2d.Lines.*;
 import static arc.math.Angles.*;
-import arc.graphics.g2d.*;
 
 import immersionIndustry.IMColors;
 
