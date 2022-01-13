322e7715ea86fa909cd320b88ddd7ba8e1ca4f72
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2022-01-13 19:24:07 +0800

    Add files via upload

--- src/immersionIndustry/types/blocks/defense/Diffuser.java
+++ src/immersionIndustry/types/blocks/defense/Diffuser.java
@@ -106,6 +106,7 @@ public class Diffuser extends ReloadTurret {
     //是否在防御的范围
     protected boolean isInRange(float to) {
       to = to - rotation;
+      Log.info("to: @",to);
       if(to >= -90 && to <= 90) return true;
       return false;
     }
