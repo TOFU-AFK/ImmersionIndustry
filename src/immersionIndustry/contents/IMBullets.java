ac3b303c7f6b1edaa834191b50184fd4cd1ca5ca
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2022-02-02 16:37:10 +0800

    Add files via upload

--- src/immersionIndustry/contents/IMBullets.java
+++ src/immersionIndustry/contents/IMBullets.java
@@ -23,10 +23,51 @@ import static arc.graphics.g2d.Draw.rect;
 import static arc.graphics.g2d.Draw.*;
 import static arc.graphics.g2d.Lines.*;
 import static arc.math.Angles.*;
- 
+
+import immersionIndustry.IMColors;
+import immersionIndustry.types.blocks.distribution.LiquidMassDriver.*;
+
 public class IMBullets implements ContentList {
+  
+  public static glowLaser;
   
 	@Override
 	public void load() {
+	  glowLaser = new ContinuousLaserBulletType(10){
+	    
+	    {
+	      colors = new Color[]{IMColors.colorPrimary,IMColors.colorDarkPrimary,Color.white};
+	    }
+	    
+	    @Override
+	    public void draw(Bullet b) {
+	      if(b.data instanceof DriverBuildData data) {
+	        float realLength = Mathf.dst(data.from,data.to);
+	        float fout = Mathf.clamp(b.time > b.lifetime - fadeTime ? 1f - (b.time - (lifetime - fadeTime)) / fadeTime : 1f);
+        float baseLen = realLength * fout;
+
+        Lines.lineAngle(b.x, b.y, b.rotation(), baseLen);
+        for(int s = 0; s < colors.length; s++){
+          Draw.color(Tmp.c1.set(colors[s]).mul(1f + Mathf.absin(Time.time, 1f, 0.1f)));
+          for(int i = 0; i < tscales.length; i++){
+            Tmp.v1.trns(b.rotation() + 180f, (lenscales[i] - 1f) * spaceMag);
+            Lines.stroke((width + Mathf.absin(Time.time, oscScl, oscMag)) * fout * strokes[s] * tscales[i]);
+            Lines.lineAngle(b.x + Tmp.v1.x, b.y + Tmp.v1.y, b.rotation(), baseLen * lenscales[i], false);
+          }
+        }
+        
+        Draw.color(data.liquid.color);
+        Angles.randLenVectors(b.id, 10, 440 * b.fin() / 2 + 460 / 2,b.rotation, 0,new Floatc2(x,y -> {
+          Lines.lineAngle(b.x + x, b.y + y, Mathf.angle(x, y),b.fslope() * 17 + 2);
+        }));
+
+        Tmp.v1.trns(b.rotation(), baseLen * 1.1f);
+
+        Drawf.light(b.team, b.x, b.y, b.x + Tmp.v1.x, b.y + Tmp.v1.y, lightStroke, lightColor, 0.7f);
+        Draw.reset();
+	      }
+	    }
+	    
+	  }
 	}
 }
