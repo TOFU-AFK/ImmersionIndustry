711f1baa141acef5e1fda459e4df3589f0845bf8
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2022-03-26 20:36:24 +0800

    Update and rename src/immersionIndustry/contents/blocks/IMBlocks.java to 源代码/沉浸工业/内容/块/IMBlocks.java

---
+++ 源代码/沉浸工业/内容/块/IMBlocks.java
@@ -0,0 +1,59 @@
+package immersionIndustry.contents.blocks;
+
+import arc.*;
+import arc.math.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import mindustry.ctype.*;
+import mindustry.content.*;
+
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.experimental.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+import immersionIndustry.contents.*;
+import immersionIndustry.contents.drawer.*;
+import immersionIndustry.types.blocks.production.*;
+import immersionIndustry.types.blocks.distribution.*;
+import immersionIndustry.types.blocks.defense.*;
+import immersionIndustry.types.blocks.power.*;
+
+//实现ContentList
+public class IMBlocks implements ContentList {
+  
+  public static Block collapseExtractor;
+  
+  //在这里创建变量
+  @Override
+  public void load() {
+    
+    collapseExtractor = new IntelligentMiningMachine("collapse-extractor"){{
+      requirements(Category.production, ItemStack.with(Items.silicon, 95));
+      consumes.power(30);
+      health = 200*size*size;
+      size = 3;
+    }};
+  }
+  
+}
