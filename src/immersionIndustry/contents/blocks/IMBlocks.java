8be4322ca50d387e2542ebcf9177bb77f97a14b9
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2022-03-26 20:40:39 +0800

    Update IMBlocks.java

--- src/immersionIndustry/contents/blocks/IMBlocks.java
+++ src/immersionIndustry/contents/blocks/IMBlocks.java
@@ -42,64 +42,14 @@ import immersionIndustry.types.blocks.power.*;
 //实现ContentList
 public class IMBlocks implements ContentList {
   
-  public static Block t1ChipFactory,collapseExtractor,collapseQuantumCultivation,auroraGuide,powerDiffuser,glowReleaser;
+  public static Block collapseExtractor;
   
   //在这里创建变量
   @Override
   public void load() {
     
-    glowReleaser = new GlowReleaser("glow-releaser"){{
-      requirements(Category.power, ItemStack.with(Items.lead, 500, Items.silicon, 300, Items.graphite, 400, Items.thorium, 100, Items.surgeAlloy, 250, Items.metaglass, 250));
-      size = 6;
-      health = 900;
-      plasmas = 5;
-      powerProduction = 360f;
-      itemDuration = 140f;
-      ambientSound = Sounds.pulse;
-      ambientSoundVolume = 0.07f;
-      consumes.liquid(Liquids.cryofluid, 0.01f);
-      consumes.power(12);
-      consumes.item(IMItems.collapseQuantum);
-    }};
-    
-    powerDiffuser = new Diffuser("power-diffuser"){{
-      health = 200*size*size;
-      size = 1;
-      requirements(Category.effect, ItemStack.with(Items.silicon, 35,Items.copper, 75,Items.lead,60,IMItems.t1BasicChip,6,IMItems.cuTiAlloy,6));
-      powerUse = 6f;
-      baseExplosiveness = 10f;
-    }};
-    
-    auroraGuide = new LaserTransmitter("aurora-guide"){{
-      health = 200*size*size;
-      size = 1;
-      requirements(Category.distribution, ItemStack.with(Items.silicon, 35,Items.copper, 75,Items.lead,60,IMItems.t1BasicChip,6,IMItems.cuTiAlloy,6));
-      consumes.power(2);
-    }};
-    
-    t1ChipFactory = new GenericCrafter("T1-chip-factory"){{
-      health = 200*size*size;
-      size = 2;
-      updateEffect = IMFx.dispersion;
-      outputItem = new ItemStack(IMItems.t1BasicChip, 1);
-      requirements(Category.crafting, ItemStack.with(Items.silicon, 45,Items.titanium, 75,Items.lead,120));
-      consumes.items(ItemStack.with(Items.silicon, 1, Items.lead, 2));
-      consumes.power(5);
-      drawer = new DrawLightBlock();
-    }};
-    
-    collapseQuantumCultivation = new GenericCrafter("collapse-quantum-cultivation"){{
-      health = 200*size*size;
-      size = 4;
-      outputItem = new ItemStack(IMItems.collapseQuantum, 1);
-      requirements(Category.crafting, ItemStack.with(Items.surgeAlloy, 25,Items.silicon, 35,Items.phaseFabric,22));
-      consumes.items(ItemStack.with(Items.plastanium, 1, Items.thorium, 2,Items.phaseFabric,1));
-      consumes.power(6);
-      drawer = new DrawSwirlBlock();
-    }};
-    
     collapseExtractor = new IntelligentMiningMachine("collapse-extractor"){{
-      requirements(Category.production, ItemStack.with(Items.silicon, 95,IMItems.thTiAlloy, 95,Items.surgeAlloy, 182,IMItems.collapseQuantum,32,IMItems.t1BasicChip,52));
+      requirements(Category.production, ItemStack.with(Items.silicon, 95));
       consumes.power(30);
       health = 200*size*size;
       size = 3;
