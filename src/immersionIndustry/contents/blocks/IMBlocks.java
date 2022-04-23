e3ed5c4715f3f1b67abb22acb401dcab3f36c4fb
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2022-04-23 17:23:56 +0800

    Add files via upload

--- src/immersionIndustry/contents/blocks/IMBlocks.java
+++ src/immersionIndustry/contents/blocks/IMBlocks.java
@@ -42,18 +42,11 @@ import immersionIndustry.types.blocks.power.*;
 //实现ContentList
 public class IMBlocks implements ContentList {
   
-  public static Block t1ChipFactory,collapseExtractor,collapseQuantumCultivation,auroraGuide,powerDiffuser,glowReleaser,transporter;
+  public static Block t1ChipFactory,collapseExtractor,collapseQuantumCultivation,auroraGuide,powerDiffuser,glowReleaser;
   
   //在这里创建变量
   @Override
   public void load() {
-    
-    transporter = new Transporter("yransporter"){{
-      health = 200*size*size;
-      size = 5;
-      requirements(Category.units, ItemStack.with(Items.silicon, 35,Items.copper, 75,Items.lead,60,IMItems.t1BasicChip,6,IMItems.cuTiAlloy,6));
-      consumes.power(6);
-    }};
     
     glowReleaser = new GlowReleaser("glow-releaser"){{
       requirements(Category.power, ItemStack.with(Items.lead, 500, Items.silicon, 300, Items.graphite, 400, Items.thorium, 100, Items.surgeAlloy, 250, Items.metaglass, 250));
